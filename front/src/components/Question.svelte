40d19a0a1738510060d7bd4c1677374794612ba8
Swann CASTEL <swann.castel@hotmail.fr>
2019-06-23 17:14:22 +0200

    use refactor + add front and docker

---
+++ front/src/components/Question.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let question;
+  export let color;
+</script>
+
+<style>
+  .question {
+    margin: 10px;
+  }
+</style>
+
+<div class="question">
+  <article class="tile is-child notification {color}">
+    <p class="subtitle">{question}</p>
+  </article>
+</div>
---
+++ front/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Question from '../components/Question.svelte';
+</script>
+
+<svelte:head>
+	<title>Qui est-ce ?</title>
+</svelte:head>
+
+<Question question="he ?" color="is-info"></Question>
+<Question question="ha ?" color="is-warning"></Question>
