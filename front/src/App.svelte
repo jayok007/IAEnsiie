6aa357e0c5736788f00d3c526c96b5ee09e5bf67
Swann CASTEL <swann.castel@hotmail.fr>
2019-06-25 11:33:20 +0200

    feat(front): add winning and losing popup

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -3,12 +3,15 @@
   import Header from './components/Header.svelte'
   import Question from './components/Question.svelte'
   import Loader from './components/Loader.svelte'
+  import Winning from './components/Winning.svelte'
+  import Losing from './components/Losing.svelte'
 
   let response = ''
   let people = null
   let questions = []
   let displayedQuestions = []
   let isWinning = false
+  let isLosing = false
   let loading = true
 
   function fetchRandomPeople() {
@@ -17,6 +20,7 @@
     questions = []
     displayedQuestions = []
     isWinning = false
+    isLosing = false
     loading = true
 
     fetch('/api/random-people')
@@ -30,7 +34,9 @@
 
   function generateQuestions() {
     let isMan = people.sexe === 'M'
-    questions = [...questions, `Je suis né le ${people.birth_date}`]
+    if (people.birth_date) {
+      questions = [...questions, `Je suis né le ${people.birth_date}`]
+    }
     questions = [...questions, `Je suis un${isMan ? '' : 'e'} ${isMan ? 'homme' : 'femme'}`]
     people.jobs.forEach(job => {
       questions = [...questions, `Je suis ${job}`]
@@ -46,7 +52,7 @@
   }
 
   function giveUp() {
-    isWinning = true
+    isLosing = true
   }
 
   function validate() {
@@ -66,10 +72,11 @@
 <main>
   <div class="container">
     {#if loading}
-    <Loader></Loader>
+      <Loader></Loader>
     {/if}
-    {#if isWinning}
-    {people.name}
+    {#if people}
+      <Winning text={people.text} name={people.name} {isWinning}></Winning>
+      <Losing text={people.text} name={people.name} {isLosing}></Losing>
     {/if}
     <input
       class="input is-info is-large"
@@ -81,21 +88,21 @@
 
     <button
       class="button is-info"
-      disabled={isWinning}
+      disabled={isWinning || isLosing}
       on:click={displayQuestion}
     >
       Nouvelle question
     </button>
     <button
       class="button is-info"
-      disabled={isWinning}
+      disabled={isWinning || isLosing}
       on:click={giveUp}
     >
       Je donne ma langue au chat
     </button>
     <button
       class="button is-info"
-      disabled={!isWinning}
+      disabled={!isWinning && !isLosing}
       on:click={fetchRandomPeople}
     >
       Recommencer
---
+++ front/src/components/Winning.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let text = ''
+  export let name = ''
+  export let isWinning = false
+</script>
+
+<div class={`modal ${isWinning ? 'is-active' : ''}`}>
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <article class="message is-success">
+      <div class="message-header">
+        <p>C'est bien {name}, vous avez gagné !!!</p>
+        <button on:click={() => isWinning = false} class="delete" aria-label="delete"></button>
+      </div>
+      <div class="message-body">
+        {text}
+      </div>
+    </article>
+  </div>
+</div>
